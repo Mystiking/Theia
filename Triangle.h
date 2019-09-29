e5287c9940d7d45a0ef1133552bb63f38ae08416
Mystiking <maxkragballe@gmail.com>
2019-09-29 16:27:41 +0200

    Add area function to triangle

--- Triangle.h
+++ Triangle.h
@@ -9,7 +9,12 @@ class Triangle {
             p[0] = v; p[1] = w; p[2] = u;
         };
 
-        friend float area() {
-            Vector3 u = *p[1] - *p[0];
+        float area() {
+            return 0.5f * sqrt(pow((p[1].y - p[0].y) * (p[2].z - p[0].z) -
+                                   (p[1].z - p[0].z) * (p[2].y - p[0].z), 2) +
+                               pow((p[1].z - p[0].z) * (p[2].x - p[0].x) -
+                                   (p[1].x - p[0].x) * (p[2].z - p[0].z), 2) +
+                               pow((p[1].x - p[0].x) * (p[2].y - p[0].y) -
+                                   (p[1].y - p[0].y) * (p[2].x - p[0].x), 2));
         }
 };
