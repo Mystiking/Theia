11b39cab868693305eb6d7cb84742bab36c2e20d
Mystiking <maxkragballe@gmail.com>
2020-03-29 21:21:14 +0200

    Add wireframe option

--- draw_triangle/render_example.cpp
+++ draw_triangle/render_example.cpp
@@ -99,25 +99,70 @@ int main() {
         6, 1, 2
     };
 
-    static const GLfloat colors[] = {
-        0.583f,  0.771f,  0.014f,
-        0.327f,  0.483f,  0.844f,
-        0.822f,  0.569f,  0.201f,
-        0.310f,  0.747f,  0.185f,
-        0.597f,  0.770f,  0.761f,
-        0.359f,  0.583f,  0.152f,
-        0.559f,  0.861f,  0.639f,
-        0.195f,  0.548f,  0.859f,
-        0.771f,  0.328f,  0.970f,
-        0.676f,  0.977f,  0.133f,
-        0.971f,  0.572f,  0.833f,
-        0.543f,  0.021f,  0.978f,
-        0.279f,  0.317f,  0.505f,
+    std::vector<unsigned short> g_line_indices_buffer_data = {
+        3, 1, 1, 0, 0, 3,
+        3, 2, 2, 1, 1, 3,
+        4, 2, 2, 3, 3, 4,
+        4, 5, 5, 2, 2, 4,
+        4, 0, 0, 7, 7, 4,
+        4, 3, 3, 0, 0, 4,
+        7, 5, 5, 4, 4, 7,
+        7, 6, 6, 5, 5, 7,
+        0, 6, 6, 7, 7, 0,
+        0, 1, 1, 6, 6, 0,
+        6, 2, 2, 5, 5, 6,
+        6, 1, 1, 2, 2, 6
     };
 
-    int color = 0;
-    GLfloat g_color_buffer_data[12*3*3];
 
+    const static GLfloat g_color_buffer_data[] = {
+    0.583f,  0.771f,  0.014f,
+    0.609f,  0.115f,  0.436f,
+    0.327f,  0.483f,  0.844f,
+    0.822f,  0.569f,  0.201f,
+    0.435f,  0.602f,  0.223f,
+    0.310f,  0.747f,  0.185f,
+    0.597f,  0.770f,  0.761f,
+    0.559f,  0.436f,  0.730f,
+    0.359f,  0.583f,  0.152f,
+    0.483f,  0.596f,  0.789f,
+    0.559f,  0.861f,  0.639f,
+    0.195f,  0.548f,  0.859f,
+    0.014f,  0.184f,  0.576f,
+    0.771f,  0.328f,  0.970f,
+    0.406f,  0.615f,  0.116f,
+    0.676f,  0.977f,  0.133f,
+    0.971f,  0.572f,  0.833f,
+    0.140f,  0.616f,  0.489f,
+    0.997f,  0.513f,  0.064f,
+    0.945f,  0.719f,  0.592f,
+    0.543f,  0.021f,  0.978f,
+    0.279f,  0.317f,  0.505f,
+    0.167f,  0.620f,  0.077f,
+    0.347f,  0.857f,  0.137f,
+    0.055f,  0.953f,  0.042f,
+    0.714f,  0.505f,  0.345f,
+    0.783f,  0.290f,  0.734f,
+    0.722f,  0.645f,  0.174f,
+    0.302f,  0.455f,  0.848f,
+    0.225f,  0.587f,  0.040f,
+    0.517f,  0.713f,  0.338f,
+    0.053f,  0.959f,  0.120f,
+    0.393f,  0.621f,  0.362f,
+    0.673f,  0.211f,  0.457f,
+    0.820f,  0.883f,  0.371f,
+    0.982f,  0.099f,  0.879f
+    };
+
+    static GLfloat g_line_colors_buffer_data[12*6*3];
+    for (int i = 0; i < 12*6*3; i++) {
+        g_line_colors_buffer_data[i] = 0.0f;
+    }
+
+    GLuint colorbuffer;
+    glGenBuffers(1, &colorbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
 
     GLuint indicesbuffer;
     glGenBuffers(1, &indicesbuffer);
@@ -128,11 +173,10 @@ int main() {
     GLuint programID = LoadShaders( "../shaders/VertexShader.glsl",
                                     "../shaders/FragmentShader.glsl");
     GLuint MatrixID = glGetUniformLocation(programID, "MVP");
-    GLuint colorbuffer;
-    glGenBuffers(1, &colorbuffer);
 
 	double lastTime = glfwGetTime();
 	int nbFrames = 0;
+    bool draw_wireframe = false;
 	do{
 		// Measure speed
 		double currentTime = glfwGetTime();
@@ -144,21 +188,6 @@ int main() {
 			lastTime += 1.0;
 		}
 
-        for (int i = 0; i < 12*3*3; i++) {
-            if (color == 0) {
-                g_color_buffer_data[i*3 + 0] = 1.0f;
-                g_color_buffer_data[i*3 + 1] = 0.0f;
-                g_color_buffer_data[i*3 + 2] = 0.0f;
-            }
-            if (color == 1) {
-                g_color_buffer_data[i*3 + 0] = 0.0f;
-                g_color_buffer_data[i*3 + 1] = 0.0f;
-                g_color_buffer_data[i*3 + 2] = 1.0f;
-            }
-        }
-        color = color == 0 ? 1 : 0;
-        glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glUseProgram(programID);
         glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &mvp[0][0]);
@@ -173,28 +202,76 @@ int main() {
                 0,
                 (void*)0
         );
-        // 2nd attribute buffer : colors
-        glEnableVertexAttribArray(1);
-        glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
-        glVertexAttribPointer(
-            1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
-            3,                                // size
-            GL_FLOAT,                         // type
-            GL_FALSE,                         // normalized?
-            0,                                // stride
-            (void*)0                          // array buffer offset
-        );
+        if (draw_wireframe) {
 
-        glBindBuffer(GL_ARRAY_BUFFER, indicesbuffer);
+            // Draw lines
+            // 2nd attribute buffer : colors
+            glEnableVertexAttribArray(1);
+            glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+            glBufferData(GL_ARRAY_BUFFER,
+                         sizeof(g_line_colors_buffer_data),
+                         g_line_colors_buffer_data,
+                         GL_STATIC_DRAW);
 
-        glDrawElements(
-                GL_TRIANGLES,
-                g_indices_buffer_data.size(),
-                GL_UNSIGNED_SHORT,
-                (void*)0
-        );
-        //glDrawArrays(GL_TRIANGLES, 0, 3);
+            glVertexAttribPointer(
+                1,                                // attribute index
+                3,                                // size
+                GL_FLOAT,                         // type
+                GL_FALSE,                         // normalized?
+                0,                                // stride
+                (void*)0                          // array buffer offset
+            );
+
+            glBindBuffer(GL_ARRAY_BUFFER, indicesbuffer);
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+                         g_indices_buffer_data.size()*sizeof(unsigned short),
+                         &g_indices_buffer_data[0],
+                         GL_STATIC_DRAW);
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+            glDrawElements(
+                    GL_TRIANGLES,
+                    g_indices_buffer_data.size(),
+                    GL_UNSIGNED_SHORT,
+                    (void*)0
+            );
+        } else {
+            // Draw polygons
+            glEnableVertexAttribArray(1);
+            glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+            glBufferData(GL_ARRAY_BUFFER,
+                         sizeof(g_color_buffer_data),
+                         g_color_buffer_data,
+                         GL_STATIC_DRAW);
+            glVertexAttribPointer(
+                1,                                // attribute index.
+                3,                                // size
+                GL_FLOAT,                         // type
+                GL_FALSE,                         // normalized?
+                0,                                // stride
+                (void*)0                          // array buffer offset
+            );
+
+            glBindBuffer(GL_ARRAY_BUFFER, indicesbuffer);
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+                         g_indices_buffer_data.size()*sizeof(unsigned short),
+                         &g_indices_buffer_data[0],
+                         GL_STATIC_DRAW);
+
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+            glEnable(GL_POLYGON_OFFSET_FILL);
+            glPolygonOffset(10.0, 10.0);
+
+            glDrawElements(
+                    GL_TRIANGLES,
+                    g_indices_buffer_data.size(),
+                    GL_UNSIGNED_SHORT,
+                    (void*)0
+            );
+            glDisable(GL_POLYGON_OFFSET_FILL);
+        }
         glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
