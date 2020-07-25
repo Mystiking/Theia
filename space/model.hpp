2638bf1bfa94e6b1d5b33c0c7e6480278b5f8f30
Max <maxkragballe@gmail.com>
2020-07-25 22:46:18 +0200

    Fix incude error

--- space/model.hpp
+++ space/model.hpp
@@ -7,8 +7,6 @@
 #ifndef LOADERINCLUDES
 #define LOADERINCLUDES
 #include "load_texture.h"
-#include "objloader.hpp"
-#include "vboindexer.hpp"
 #endif
 #include <vector>
 
@@ -26,27 +24,5 @@ class Model {
         GLuint shader_id;
 
         Model() {}
-
-        Model(std::string model_file, const char* texture_file, GLuint shader_id, glm::mat4 model_matrix = glm::mat4(1.0f)) {
-            /* Set skin*/
-            std::vector<glm::vec3> _vertices;
-            std::vector<glm::vec2> _uvs;
-            std::vector<glm::vec3> _normals;
-            // Get vertices, uvs and normals
-            if (model_file.substr(model_file.length() - 3, 3) == "obj"){
-                loadOBJ(model_file.c_str(), _vertices, _uvs, _normals);
-                // Set all buffers
-                indexVBO(_vertices, _uvs, _normals, this->indices, this->vertices, this->uvs, this->normals);
-            } else {
-                std::cout << "Unsupported model file type.\n";
-                exit(-1);
-            }
-            /* Set texture file */
-            this->texture = load_texture(texture_file, shader_id);
-            /* Set GLSL program reference */
-            this->shader_id = shader_id;
-            /* Set model matrix */
-            this->model_matrix = model_matrix;
-        }
 };
 
